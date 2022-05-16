8dd3776230a14378a66845286fc0f3967186c1f1
Daryl <darylcecile@gmail.com>
2022-05-16 17:51:57 +0100

    updated tests

--- _campus/test.js
+++ _campus/test.js
@@ -7,9 +7,9 @@
  */
 
 describe("must have at least 5 frames", ()=>{
-	expect(frameInstructions).to.be.above(4);
+	expect(window.frameInstructions).to.be.above(4);
 });
 
 describe("framerate must not exceed 30 FPS", ()=>{
-	expect(framesPerSecond).to.be.below(31)
+	expect(window.framesPerSecond).to.be.below(31)
 });
